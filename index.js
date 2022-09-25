c9a0805986ba36b0ccf00471c4f658bfa008235e
korodoff <rudoffsutnga368@gmail.com>
2022-09-25 22:08:26 +0530

    server socket

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { Server } = require("socket.io");
 
 
+app.use(cors());
+const server = http.createServer(app);
 
-app.get("/",(req,res)=>{
-    res.send("HElloo")
-    console.log("working")
-})
+const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"],
+    },
+  });
+  
+  io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+  
+    socket.on("join_room", (data) => {
+      socket.join(data);
+      console.log(`User with ID: ${socket.id} joined room: ${data}`);
+    });
+  
+    socket.on("send_message", (data) => {
+      socket.to(data.room).emit("receive_message", data);
+    });
+  
+    socket.on("disconnect", () => {
+      console.log("User Disconnected", socket.id);
+    });
+  });
+  
+  server.listen(3001, () => {
+    console.log("SERVER RUNNING");
+  });
+
+// app.get("/",(req,res)=>{
+//     res.send("HElloo")
+//     console.log("working")
+// })
 
 
-app.listen(9090,()=>{
-    console.log("server Working")
-})
+// app.listen(9090,()=>{
+//     console.log("server Working")
+// })
